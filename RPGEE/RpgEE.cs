1c935696c39e899c84140d767ac7099d8af19937
Alberto <as12015@ic.ac.uk>
2017-08-17 09:38:36 +0100

    RpgEE.cs: Add Generator class dependencies to main RPGEE class to dynamically generate UI components

--- RPGEE/RpgEE.cs
+++ RPGEE/RpgEE.cs
@@ -16,6 +16,32 @@ namespace RPGEE
         {
             InitializeComponent();
             this.Text = "RpgEE";
+
+            /** Generates a table to dock home Button Components
+             * Table layout:
+             * 
+             * +----------+----------+----------+
+             * |          |          |          |
+             * |          |          |          |
+             * |          |          |          |
+             * +----------+----------+----------+
+             * |          |          |          |
+             * |          |          |          |
+             * |          |          |          | 
+             * +----------+----------+----------+ 
+             */
+
+            TableLayoutPanel homeTable = Generator<TableLayoutPanel>.generateHomeTable(this, 2, 3);
+
+            Button mapBtn = Generator<Button>.addObject(new Button() { Text = "Map" }, homeTable, 0, 0);
+            Button playersBtn = Generator<Button>.addObject(new Button() { Text = "Players" }, homeTable, 1, 0);
+            Button areaBtn = Generator<Button>.addObject(new Button() { Text = "Area" }, homeTable, 2, 0);
+            Button rulesBtn = Generator<Button>.addObject(new Button() { Text = "Rules" }, homeTable, 0, 1);
+            Button spritesBtn = Generator<Button>.addObject(new Button() { Text = "Sprites" }, homeTable, 1, 1);
+            Button customBtn = Generator<Button>.addObject(new Button() { Text = "Custom" }, homeTable, 2, 1);
+           
+
+            homeTable.ResumeLayout();
         }
 
         
