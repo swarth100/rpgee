e9a5679dd2d92abe7a9fda91dd075c2cddeaa87f
Alberto <as12015@ic.ac.uk>
2017-08-18 14:10:35 +0100

    Program.cs: Remove autogenerated comment

--- RPGEE/Program.cs
+++ RPGEE/Program.cs
@@ -8,15 +8,14 @@ namespace RPGEE
 {
     static class Program
     {
-        /// <summary>
-        /// Punto di ingresso principale dell'applicazione.
-        /// </summary>
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new RpgEE());
+
+
         }
     }
 }
