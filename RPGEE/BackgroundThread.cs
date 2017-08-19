9aaec0b5c80e98263c28d1562443fc5171290750
Alberto <as12015@ic.ac.uk>
2017-08-19 21:08:59 +0100

    BackgroundThread.cs: Add computationalThread to execute initParsing and handle map rendering

--- RPGEE/BackgroundThread.cs
+++ RPGEE/BackgroundThread.cs
@@ -11,14 +11,96 @@ namespace RPGEE
 {
     class BackgroundThread
     {
+        public enum Actions
+        {
+            None,
+            InitParse,
+        }
+
+        /* Class used when signalling the computational thread with actions */
+        public class ActionEvent
+        {
+            public Actions action { get; set; }
+            public Object data { get; set; }
+
+            public ActionEvent(Actions action, Object data)
+            {
+                this.action = action;
+                this.data = data;
+            }
+        }
+
+
         /* Public objects held by BackgroundThreads */
         public static PhysicsWorld Physicsworld = new PhysicsWorld();
         public static Dictionary<int, Player> Players = new Dictionary<int, Player>();
+
+        /* Thread communication object */
+        public static Queue<ActionEvent> activityQueue = new Queue<ActionEvent>();
+        public static Object _activityLock = new Object();
+
+        /* RoomData object */
+        public static uint[,,] roomData;
+        public static Object _roomDataLock = new Object();
 
         /* Class fields */
         private static string email = "";
         private static string password = "";
         private static string roomid = "";
+
+        public static void runComputationsThread()
+        {
+            ActionEvent activity;
+            while (true)
+            {
+                lock (_activityLock)
+                {
+                    if (activityQueue.Count != 0)
+                    {
+                        activity = activityQueue.Dequeue();
+                    }
+                    else
+                    {
+                        activity = null;
+                    }
+                }
+
+                if (activity != null)
+                {
+                    switch (activity.action)
+                    {
+                        case Actions.None:
+                            break;
+                        case Actions.InitParse:
+                            /* Cast data back to message type */
+                            Message e = (Message)activity.data;
+
+                            /** Initialise and populate roomData 3D array
+                             * Credits to:
+                             * https://gist.github.com/Yonom/3c9ebfe69b1432452f9b */
+                            lock (_roomDataLock)
+                            {
+                                roomData = new uint[2, e.GetInt(18), e.GetInt(19)];
+                                DataChunk[] chunks = InitParse.Parse(e);
+                                foreach (var chunk in chunks)
+                                    foreach (var pos in chunk.Locations)
+                                        roomData[chunk.Layer, pos.X, pos.Y] = chunk.Type;
+                            }
+
+                            /*
+                            Console.WriteLine(roomData[0, 0, 0]);
+                            Console.WriteLine(roomData[1, 0, 0]);
+                            Console.WriteLine(roomData[0, 1, 1]);
+                            */
+
+                            /* Force initial map async rendering */
+                            Map.loadMap(RpgEE.mapPct);
+
+                            break;
+                    }
+                }
+            }
+        }
 
         /** Most of the following code was kindly uploaded by CAPASHA on:
          * https://pastebin.com/maiaKcRD */
@@ -47,6 +129,13 @@ namespace RPGEE
                             case "init":
                                 Players.Add(m.GetInt(5), new Player(m.GetInt(5), m.GetString(13)));
                                 con.Send("init2");
+
+                                /* Add initParse event to computationsThread */
+                                lock (_activityLock)
+                                {
+                                    activityQueue.Enqueue(new ActionEvent(Actions.InitParse, m));
+                                }
+
                                 break;
                             case "init2":
                                 /* Force UI update to render home screen */
