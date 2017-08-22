c1b26037c37bd91236ae600604f4d07a7c158764
Alberto <as12015@ic.ac.uk>
2017-08-22 10:03:48 +0100

    DraggablePictureBox.cs: Add inspect and erase event handling

--- RPGEE/DraggablePictureBox.cs
+++ RPGEE/DraggablePictureBox.cs
@@ -8,27 +8,41 @@ using System.Windows.Forms;
 
 namespace RPGEE
 {
-    class DraggablePictureBox : PictureBox
+    public class DraggablePictureBox : PictureBox
     {
         private bool Dragging;
         private bool Drawing;
+        private bool Erasing;
+        public bool Inspecting { get; set; }
         private int newTopPos;
         private int newLeftPos;
         private int maxRightPos;
         private int maxBottomPos;
         private readonly DragMousePosition mousePos;
+        private readonly DragMousePosition mouseDrag;
         private readonly Map map;
+
+        private readonly ToolTip inspectTt;
 
         private class DragMousePosition
         {
             public int X { get; set; }
             public int Y { get; set; }
+
+            public void updatePosition(MouseEventArgs e)
+            {
+                X = e.X;
+                Y = e.Y;
+            }
         }
 
         public DraggablePictureBox(Map map) : base()
         {
             this.map = map;
+
+            inspectTt = new ToolTip();
             mousePos = new DragMousePosition();
+            mouseDrag = new DragMousePosition();
 
             this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.OnMouseDown);
             this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.OnMouseMove);
@@ -40,6 +54,7 @@ namespace RPGEE
         {
             Dragging = false;
             Drawing = false;
+            Erasing = false;
         }
 
         /* Handles mouse button clicking */
@@ -50,8 +65,9 @@ namespace RPGEE
                 if (map.status == Map.Status.Move)
                 {
                     Dragging = true;
-                    mousePos.X = e.X;
-                    mousePos.Y = e.Y;
+
+                    mousePos.updatePosition(e);
+                    mouseDrag.updatePosition(e);
 
                     maxBottomPos = RpgEE.getMapHeight();
                     maxRightPos = RpgEE.getMapWidth();
@@ -60,13 +76,18 @@ namespace RPGEE
                 {
                     Drawing = true;
                 }
+                else if (map.status == Map.Status.Delete)
+                {
+                    Erasing = true;
+                }
             }
         }
 
         private void OnMouseMove(object sender, MouseEventArgs e)
         {
             Control c = sender as Control;
-            if (c != null)
+
+            if (c != null && (mouseDrag.X != e.X || mouseDrag.Y != e.Y))
             {
                 if (Dragging)
                 {
@@ -82,6 +103,16 @@ namespace RPGEE
                 {
                     RpgEE.map.drawPoint(new Point(e.X, e.Y));
                 }
+                else if (Erasing)
+                {
+                    RpgEE.map.erasePoint(new Point(e.X, e.Y));
+                }
+                else if (Inspecting)
+                {
+                    RpgEE.map.showTooltip(new Point(e.X, e.Y));
+                }
+
+                mouseDrag.updatePosition(e);
             }
         }
     }
